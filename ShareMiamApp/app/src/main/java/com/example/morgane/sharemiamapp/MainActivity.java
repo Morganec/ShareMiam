37761aea5a9f7233e526ec5c7c7ed016d2c3ba29
morgane <mcabrol21@gmail.com>
2017-11-10 14:47:52 -0500

    google map

--- ShareMiamApp/app/src/main/java/com/example/morgane/sharemiamapp/MainActivity.java
+++ ShareMiamApp/app/src/main/java/com/example/morgane/sharemiamapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.morgane.sharemiamapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button loginButton = (Button) findViewById(R.id.seeMapBtn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
