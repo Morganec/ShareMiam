8a9e5515ebf15888b7ae5de6da1c4fef8cca17c5
morgane <mcabrol21@gmail.com>
2017-11-10 18:06:43 -0500

    Take a picture

--- ShareMiamApp/app/src/main/java/com/example/morgane/sharemiamapp/AddItemActivity.java
+++ ShareMiamApp/app/src/main/java/com/example/morgane/sharemiamapp/AddItemActivity.java
@@ -1,13 +1,35 @@
 package com.example.morgane.sharemiamapp;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddItemActivity extends AppCompatActivity {
-
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+
+        final Button btnTakePict = (Button) findViewById(R.id.btnTakePicture);
+        btnTakePict.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+               dispatchTakePictureIntent();
+            }
+        });
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
