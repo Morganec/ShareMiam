1bd794cd3baaa0eaeaead3f08e1075dbfd7d597c
morgane <mcabrol21@gmail.com>
2017-11-12 19:06:12 -0500

    "Ajout item en BDD"

--- ShareMiamApp/app/src/main/java/com/example/morgane/sharemiamapp/AddItemActivity.java
+++ ShareMiamApp/app/src/main/java/com/example/morgane/sharemiamapp/AddItemActivity.java
@@ -11,13 +11,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class AddItemActivity extends AppCompatActivity {
@@ -43,23 +49,26 @@ public class AddItemActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                EditText edtTitre = (EditText) findViewById(R.id.edtTitleItem);
-                TextView twDescr = (TextView) findViewById(R.id.twDescrAdress);
-                EditText edtCodePostal = (EditText) findViewById(R.id.edtCodePostal);
-                EditText edtRue = (EditText) findViewById(R.id.edtRue);
+                CheckBox chbxConfirm = (CheckBox) findViewById(R.id.chbxConfirm);
 
-                String titre = edtTitre.getText().toString();
-                String descr = twDescr.getText().toString();
-                String adresse = edtRue.getText().toString() + " " + edtCodePostal.getText().toString();
+                if(chbxConfirm.isChecked()){
 
-                Context context = getApplicationContext();
-                CharSequence text = "Element ajouté";
-                int duration = Toast.LENGTH_SHORT;
+                    addItem();
+                    Context context = getApplicationContext();
+                    CharSequence text = "Element ajouté";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-                Intent intent = new Intent(AddItemActivity.this, MainActivity.class);
-                startActivity(intent);
+                    Intent intent = new Intent(AddItemActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Context context = getApplicationContext();
+                    CharSequence text = "Veuillez confirmer que vous avez lu les conditions d'utilisations";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
 
             }
         });
@@ -74,6 +83,31 @@ public class AddItemActivity extends AppCompatActivity {
 
     }
 
+
+    private void  addItem(){
+
+        EditText edtTitle = (EditText) findViewById(R.id.edtTitleItem);
+        EditText edtDescr = (EditText) findViewById(R.id.edtDescriptionItem);
+        EditText edtPostalCode = (EditText) findViewById(R.id.edtCodePostal);
+        EditText edtStreet = (EditText) findViewById(R.id.edtRue);
+        EditText edtDate = (EditText) findViewById(R.id.edtValidityDate);
+
+
+        String validityDate = edtDate.getText().toString();
+        String title = edtTitle.getText().toString();
+        String descr = edtDescr.getText().toString();
+        String street = edtStreet.getText().toString();
+        String postalCode = edtPostalCode.getText().toString();
+
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference mDatabaseReference = mFirebaseDatabase.getReference("food");
+        String uid = auth.getCurrentUser().getUid();
+        String id= mDatabaseReference.push().getKey();
+        Food f = new Food(uid,title,descr,street,postalCode,validityDate);
+        mDatabaseReference.child(id).setValue(f);
+    }
 
 
 
---
+++ ShareMiamApp/app/src/main/java/com/example/morgane/sharemiamapp/Food.java
@@ -0,0 +1,27 @@
+package com.example.morgane.sharemiamapp;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * Created by morgane on 12/11/2017.
+ */
+
+public class Food {
+    public String uid;
+    public boolean deleted = false;
+    public String title;
+    public String description;
+    public String street;
+    public String postalCode;
+    public String validityDate;
+
+    public Food(String uid, String title, String descr , String street, String postalCode, String validityDate ){
+        this.uid = uid;
+        this.title = title;
+        this.description = descr;
+        this.street = street;
+        this.postalCode = postalCode;
+        this.validityDate = validityDate;
+    }
+}
