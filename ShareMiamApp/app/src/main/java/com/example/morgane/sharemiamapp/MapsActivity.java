ff93f6c12da2935bbeb5b1cef12bb6ffb64b74c7
morgane <mcabrol21@gmail.com>
2017-11-14 15:14:59 -0500

    Preparation ajout image on marker

--- ShareMiamApp/app/src/main/java/com/example/morgane/sharemiamapp/MapsActivity.java
+++ ShareMiamApp/app/src/main/java/com/example/morgane/sharemiamapp/MapsActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -151,7 +152,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         for (final Food food :listFood) {
             LatLng foodAdress = getLatAndLngFromAddress(food.street + " " + food.postalCode + " " + food.pays);
             if(foodAdress != null){
-                Marker marker = mMap.addMarker(new MarkerOptions().position(foodAdress).title(food.title + ": \n Cliquer pour +infos"));
+                Marker marker = mMap.addMarker(new MarkerOptions()
+                        .position(foodAdress)
+                        .title(food.title + ": \n Cliquer pour +infos")
+                );
+
+// .icon(BitmapDescriptorFactory.fromResource(R.drawable.noimage)
                 marker.setTag(food);
                 mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
                     @Override
